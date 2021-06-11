535fab8bcddf0eaf8a6ac28603db95f6eff01571
jakehobbs <jakehobbs@gmail.com>
2021-06-10 18:52:09 -0700

    fix calendar

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -7,6 +7,9 @@ import { scheduleData } from "../mock-data/schedule";
 import CalendarStrip from "react-native-calendar-strip";
 import moment from "moment";
 
+const CONFERENCE_START_DATE = moment("2021-09-24");
+const CONFERENCE_END_DATE = moment("2021-09-30");
+
 const Stack = createStackNavigator();
 
 export default function ScheduleStack() {
@@ -30,10 +33,12 @@ export default function ScheduleStack() {
   );
 }
 
+// TODO: clean up how events are filtered so there is no duplicate code
+
 function ScheduleScreen() {
   const [refreshing, setRefreshing] = React.useState(false);
-  const [today, setToday] = React.useState(moment())
-  const [selectedDate, setSelectedDate] = React.useState(moment())
+  const [selectedDate, setSelectedDate] = React.useState(CONFERENCE_START_DATE);
+  const [today, setToday] = React.useState(moment());
   const [schedule, setSchedule] = React.useState<any>([]);
   const [filteredSchedule, setFilteredSchedule] = React.useState<any>([]);
   const calendarStrip = useRef<any>();
@@ -45,28 +50,33 @@ function ScheduleScreen() {
   };
 
   const onDateSelected = (date: moment.Moment) => {
-    setFilteredSchedule(schedule.filter((item: any) => {
-      const localDate = moment(moment(item.start_time).utc(item.start_time).toDate()).local().format("YYYY-MM-DD")
-      return localDate === date.format("YYYY-MM-DD");
-    }))
-    setSelectedDate(date)
+    setSelectedDate(date);
+    setFilteredSchedule(
+      schedule.filter((item: any) => {
+        const localDate = moment(moment(item.start_time).utc(item.start_time).toDate()).local().format("YYYY-MM-DD");
+        return localDate === date.format("YYYY-MM-DD");
+      })
+    );
   };
 
-  const initialSelectedDate = () => {
+  const initSelectedDate = () => {
     const now = moment();
-    const conferenceStartDate = moment("2021-09-24");
-    return now.isBefore(conferenceStartDate) ? conferenceStartDate : now;
+    // TODO: update "now" in state as well & set up an interval in the useEffect to watch it
+    return now.isBefore(CONFERENCE_START_DATE) ? CONFERENCE_START_DATE : now;
   };
 
   useEffect(() => {
     // TODO: load from API instead of using mock data
     setSchedule(scheduleData);
-    // TODO: automatically filter events on load based on currently selected day
-    setFilteredSchedule(schedule.filter((item: any) => {
-      const localDate = moment(moment(item.start_time).utc(item.start_time).toDate()).local().format("YYYY-MM-DD")
-      return localDate === selectedDate.format("YYYY-MM-DD");
-    }))
-  }, [schedule]);
+    const initDate = initSelectedDate();
+    setSelectedDate(initDate);
+    setFilteredSchedule(
+      scheduleData.filter((item: any) => {
+        const localDate = moment(moment(item.start_time).utc(item.start_time).toDate()).local().format("YYYY-MM-DD");
+        return localDate === initDate.format("YYYY-MM-DD");
+      })
+    );
+  }, []);
 
   // TODO: Advance the selected date if midnight has past (or maybe whenever app enters the foreground).
 
@@ -78,28 +88,41 @@ function ScheduleScreen() {
     >
       <View style={{ flex: 1 }}>
         <CalendarStrip
-          style={{ height: 75, paddingTop: 0, paddingBottom: 10, borderBottomWidth: 1, borderColor: colors.lightgrey }}
+          style={{
+            height: 90,
+            paddingTop: 0,
+            paddingBottom: 10,
+            borderBottomWidth: 1,
+            borderColor: colors.lightgrey,
+          }}
           calendarHeaderStyle={{ color: colors.blue }}
           dateNumberStyle={{ color: colors.blue }}
           dateNameStyle={{ color: colors.blue }}
-          highlightDateNameStyle={{ color: colors.white, backgroundColor: colors.blue, width: 50, paddingTop: 5 }}
-          highlightDateNumberStyle={{ color: colors.white, backgroundColor: colors.blue, width: 50, paddingBottom: 5 }}
+          highlightDateNameStyle={{ color: colors.white }}
+          highlightDateNumberStyle={{
+            color: colors.white,
+          }}
+          //@ts-ignore (valid type options seem incorrect for daySelectionAnimation)
+          daySelectionAnimation={{ type: "background", highlightColor: colors.blue }}
           scrollable={false}
-          startingDate={moment("2021-09-24").toDate()}
+          startingDate={CONFERENCE_START_DATE}
           // useIsoWeekday starts the strip on the startingDate instead of on Sunday/Monday.
           useIsoWeekday={false}
-          minDate={moment("2021-09-24").toDate()}
-          maxDate={moment("2021-09-30").toDate()}
-          selectedDate={initialSelectedDate()}
+          minDate={CONFERENCE_START_DATE}
+          maxDate={CONFERENCE_END_DATE}
+          selectedDate={selectedDate}
           onDateSelected={onDateSelected}
           ref={calendarStrip}
         />
       </View>
       {/*// TODO: filter schedule based on selected date (use state for selected date)*/}
-      {filteredSchedule
-        .map((item: any) => {
-          return <Text key={item.id}>{item.name} | {item.start_time}</Text>;
-        })}
+      {filteredSchedule.map((item: any) => {
+        return (
+          <Text key={item.id}>
+            {item.name} | {item.start_time}
+          </Text>
+        );
+      })}
     </ScrollView>
   );
 }
